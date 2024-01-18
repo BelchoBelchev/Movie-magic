7cf51bb622fb74662d4e21dcfb8e9413350e8bc3
Belcho Belchev <whitybelchev@gmail.com>
2024-01-18 21:25:44 +0200

    Add hardcoded movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,14 @@
-const movies = [];
+const movies = [{
+    title: 'Jungle Cuise',
+    genre: 'Adventure',
+    director: 'Belcho Belchev',
+    year: '2024',
+    imageUrl: '/img/jungle-cruise.jpeg',
+    rating: '4',
+    description: 'Jungle action/comedy'
+}];
 
 exports.create = (movieData) => {
     movies.push(movieData);
+    console.log(movieData);
 }; 
