0071d01fe0ce57bf9372d87b7b39f99088e27359
Belcho Belchev <whitybelchev@gmail.com>
2024-01-18 21:05:29 +0200

    Setup middleware for create post

--- src/controllers/createController.js
+++ src/controllers/createController.js
@@ -2,6 +2,13 @@ const router = require('express').Router();
 
 router.get('/create', (req, res) => {
     res.render('create');
-})
+});
+
+router.post('/create', (req, res) => {
+  
+  
+        
+    res.render();
+});
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: false }));
+
 app.use(routes);
 
 app.listen(PORT, () => {
