d6fda10025048526ef97dbf31626aabdfd53d5ad
Belcho Belchev <whitybelchev@gmail.com>
2024-01-18 21:00:29 +0200

    Setup search page

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const createController = require('./controllers/createController');
+const searchController = require('./controllers/searchController');
 
 router.use(homeController);
 router.use(createController);
+router.use(searchController);
 
 router.get('*', (req, res) => {
     res.redirect('/404');
