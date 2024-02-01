e6fe9668f508c773201ace471b1cdfdf225bb160
Belcho Belchev <whitybelchev@gmail.com>
2024-02-01 21:31:54 +0200

    Setup cast to detail page

--- src/services/castService.js
+++ src/services/castService.js
@@ -9,3 +9,9 @@ exports.getAll = () => {
     const result = Cast.find();
     return result;
 }
+
+exports.getByIds = (castIds) => {
+    const casts = Cast.find({ _id: { $in: castIds } });
+
+    return casts;
+};
