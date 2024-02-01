9cafbc660ac4a7ea8238b3e1b9b668189f72316e
Belcho Belchev <whitybelchev@gmail.com>
2024-02-01 19:24:18 +0200

    Setup cast page

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const createController = require('./controllers/createController');
 const searchController = require('./controllers/searchController');
+const createCastController = require('./controllers/createCastController');
 
 router.use(homeController);
 router.use(createController);
 router.use(searchController);
+router.use(createCastController);
 
 router.get('*', (req, res) => {
     res.redirect('/404');
