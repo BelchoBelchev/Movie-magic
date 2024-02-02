be5394a7e03e1defe89519b94f7a70ae107c12dc
Belcho Belchev <whitybelchev@gmail.com>
2024-02-02 19:38:30 +0200

    Setup register page

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ const homeController = require('./controllers/homeController');
 const createController = require('./controllers/createController');
 const searchController = require('./controllers/searchController');
 const createCastController = require('./controllers/createCastController');
+const authController = require('./controllers/authController');
 
 router.use(homeController);
 router.use(createController);
 router.use(searchController);
 router.use(createCastController);
+router.use(authController);
 
 router.get('*', (req, res) => {
     res.redirect('/404');
