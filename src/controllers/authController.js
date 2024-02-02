efa29ac515389cf2624d516dfdbde17aba7c2775
Belcho Belchev <whitybelchev@gmail.com>
2024-02-02 19:59:51 +0200

    Setup edit page

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 
+const movieService = require('../services/movieService');
+
 router.get('/register', (req, res) => {
     res.render('register');
 });
 
 router.get('/login', (req, res) => {
     res.render('login');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const movie = await movieService.getOne(req.params.id).lean();
+    
+    res.render('edit', { movie });
 });
 
 module.exports = router;
