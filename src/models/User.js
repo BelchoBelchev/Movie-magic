4a59f18bef2ec278719b9715687b761169351331
Belcho Belchev <whitybelchev@gmail.com>
2024-02-02 20:38:57 +0200

    Hash the password

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
     email: {
@@ -10,6 +11,11 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
     }
+});
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 12);
+    this.password = hash;
 });
 
 const User = mongoose.model('User', userSchema);
